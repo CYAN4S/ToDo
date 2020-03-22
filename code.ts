458bbec4145bd4fcc95e2a22d1f51a5f67b9e174
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2020-03-22 17:56:24 +0900

    apply ts

--- code.ts
+++ code.ts
@@ -1,37 +1,47 @@
-var userInput = document.getElementById("userInput");
-var addButton = document.getElementById("addButton");
-var toDoList = document.getElementById("toDoList");
-function closeList(params) {
+const userInput = <HTMLInputElement>document.getElementById("userInput");
+const addButton = <HTMLButtonElement>document.getElementById("addButton");
+const toDoList = <HTMLUListElement>document.getElementById("toDoList");
+
+function closeList(params: HTMLLIElement) {
     toDoList.removeChild(params);
 }
-function checkList(params) {
+
+function checkList(params: HTMLLIElement) {
     if (params.className === "list") {
         params.className = "list done";
-    }
-    else {
+    } else {
         params.className = "list";
     }
 }
-function addList() {
-    var inputString = userInput.value;
-    if (inputString === "") {
-        alert("You must write something.");
+
+function addList(): boolean {
+    let inputString = userInput.value;
+
+    if (inputString === ``) {
+        alert(`You must write something.`);
         return false;
     }
-    var li = document.createElement("li");
+
+    let li = document.createElement("li");
     li.className = "list";
     li.onclick = function () { checkList(li); };
+
     li.appendChild(document.createTextNode(inputString));
-    var bt = document.createElement("a");
+
+    let bt = document.createElement("a");
     bt.appendChild(document.createTextNode(" 삭제"));
     bt.onclick = function () { closeList(li); };
     bt.className = "closeButton";
     li.appendChild(bt);
+
     toDoList.appendChild(li);
+
     userInput.value = "";
+
     return true;
 }
-userInput.addEventListener("keypress", function (event) {
+
+userInput.addEventListener("keypress", function (event: KeyboardEvent) {
     if (event.keyCode === 13) {
         event.preventDefault();
         addButton.click();
